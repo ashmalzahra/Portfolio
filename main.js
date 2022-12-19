a7d05fed369d0e16339de691c9dec14aa509f690
Alhassan Osman <oalhassan847@gmail.com>
2022-12-19 20:46:03 +0000

    fix menu button click

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const menuBtn = document.querySelector(".menu")
 const closeBtn = document.querySelector(".close")
 const mobileMenu = document.querySelector(".mobile-menu")
-const mobileMenuItem = document.querySelectorAll(".mobile-menu-item")
+const mobileMenuItem1 = document.querySelector(".mobile-menu-item1")
+const mobileMenuItem2 = document.querySelector(".mobile-menu-item2")
+const mobileMenuItem3 = document.querySelector(".mobile-menu-item3")
 
 
 function showMenu(e){
-    e.preventDefault();
     mobileMenu.style.display = "block";
+    menuBtn.style.display = "none";
 }
 
 function hideMenu(){
     mobileMenu.style.display = "none";
+    menuBtn.style.display = "block";
 }
 
 menuBtn.addEventListener("click", showMenu)
 closeBtn.addEventListener("click", hideMenu)
-mobileMenuItem.addEventListener("click", hideMenu)
+mobileMenuItem1.addEventListener("click", hideMenu)
+mobileMenuItem2.addEventListener("click", hideMenu)
+mobileMenuItem3.addEventListener("click", hideMenu)
