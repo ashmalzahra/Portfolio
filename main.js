7812f788aeeda60a838730bc69da25a0aa465774
ashmalzahra <ashmal.zahraa@gmail.com>
2022-12-20 02:51:48 +0500

    Styling menu bar

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-const menuBtn = document.querySelector(".menu")
-const closeBtn = document.querySelector(".close")
-const mobileMenu = document.querySelector(".mobile-menu")
-const mobileMenuItem1 = document.querySelector(".mobile-menu-item1")
-const mobileMenuItem2 = document.querySelector(".mobile-menu-item2")
-const mobileMenuItem3 = document.querySelector(".mobile-menu-item3")
+const menuBtn = document.querySelector('.menu');
+const closeBtn = document.querySelector('.close');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuItem1 = document.querySelector('.mobile-menu-item1');
+const mobileMenuItem2 = document.querySelector('.mobile-menu-item2');
+const mobileMenuItem3 = document.querySelector('.mobile-menu-item3');
 
-
-function showMenu(e){
-    mobileMenu.style.display = "block";
-    menuBtn.style.display = "none";
+function showMenu() {
+  mobileMenu.style.display = 'block';
+  menuBtn.style.display = 'none';
 }
 
-function hideMenu(){
-    mobileMenu.style.display = "none";
-    menuBtn.style.display = "block";
+function hideMenu() {
+  mobileMenu.style.display = 'none';
+  menuBtn.style.display = 'block';
 }
 
-menuBtn.addEventListener("click", showMenu)
-closeBtn.addEventListener("click", hideMenu)
-mobileMenuItem1.addEventListener("click", hideMenu)
-mobileMenuItem2.addEventListener("click", hideMenu)
-mobileMenuItem3.addEventListener("click", hideMenu)
+menuBtn.addEventListener('click', showMenu);
+closeBtn.addEventListener('click', hideMenu);
+mobileMenuItem1.addEventListener('click', hideMenu);
+mobileMenuItem2.addEventListener('click', hideMenu);
+mobileMenuItem3.addEventListener('click', hideMenu);
