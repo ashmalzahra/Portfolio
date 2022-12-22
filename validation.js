698bc5f9f50510de79bb5ccc66bb1487472621e7
ashmalzahra <ashmal.zahraa@gmail.com>
2022-12-22 06:06:21 +0500

    Javascript validation

--- validation.js
+++ validation.js
@@ -3,8 +3,8 @@ const { email } = formContact.elements;
 const formMessage = document.querySelector(".form_message");
 const err = {Email_lowerCase: 'Email should be in lower case'};
 
-const showMessage = (error, message) => {formMessage.classList.remove('d-none')
-if (error){
+const showMessage = (isError, message) => {formMessage.classList.remove('d-none')
+if (isError){
     formMessage.classList.remove('formValid')
     formMessage.classList.add('formError')
 }  
@@ -13,5 +13,16 @@ else
     formMessage.classList.add('formValid')
     formMessage.classList.remove('formError')
 } 
-formMessage.textContent = message;
+    formMessage.textContent = message;
  }
+
+ formContact.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(email.value === email.value.toLowerCase()){
+        showMessage(false, 'Form Validation Succeeded.');
+        formContact.submit();
+        return;
+    }
+
+    showMessage(true, err.Email_lowerCase);
+ })
